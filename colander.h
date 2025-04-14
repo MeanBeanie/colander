6e2756cde15c89641d7fead512101ae8299e855a
MeanBeanie <itsawildveg@gmail.com>
2025-04-13 20:36:57 -0700

    Added mouse delta and mouse button support

--- colander.h
+++ colander.h
@@ -14,6 +14,11 @@ typedef struct {
 	int keyState[0xFF];
 	int oldKeyState[0xFF];
 
+	int mbState[3];
+	int oldMbState[3];
+	int mouseDX;
+	int mouseDY;
+
 	struct libinput* li;
 	struct udev* udev;
 } ColanderListener;
@@ -40,6 +45,12 @@ COLANDERDEF int colander_getKey(ColanderListener listener, int key);
 COLANDERDEF int colander_getKeyPressed(ColanderListener listener, int key);
 COLANDERDEF int colander_getKeyReleased(ColanderListener listener, int key);
 
+COLANDERDEF int colander_getMouseDX(ColanderListener listener);
+COLANDERDEF int colander_getMouseDY(ColanderListener listener);
+COLANDERDEF int colander_getMouseButton(ColanderListener listener, int button);
+COLANDERDEF int colander_getMouseButtonPressed(ColanderListener listener, int button);
+COLANDERDEF int colander_getMouseButtonReleased(ColanderListener listener, int button);
+
 #endif // COLANDER_H
 #ifdef COLANDER_IMPLEMENTATION
 
@@ -50,6 +61,14 @@ COLANDERDEF void colander_startListener(ColanderListener* listener){
 		listener->keyState[i] = 0;
 		listener->oldKeyState[i] = 0;
 	}
+	listener->mbState[0] = 0;
+	listener->mbState[1] = 0;
+	listener->mbState[2] = 0;
+	listener->oldMbState[0] = 0;
+	listener->oldMbState[1] = 0;
+	listener->oldMbState[2] = 0;
+	listener->mouseDX = 0;
+	listener->mouseDY = 0;
 	libinput_udev_assign_seat(listener->li, "seat0");
 	libinput_dispatch(listener->li);
 }
@@ -58,13 +77,35 @@ COLANDERDEF void colander_nextEvent(ColanderListener* listener){
 	for(int i = 0; i < 0xff; i++){
 		listener->oldKeyState[i] = listener->keyState[i];
 	}
+	listener->oldMbState[0] = listener->mbState[0];
+	listener->oldMbState[1] = listener->mbState[1];
+	listener->oldMbState[2] = listener->mbState[2];
 	event = libinput_get_event(listener->li);
-	if(event != NULL && libinput_event_get_type(event) == LIBINPUT_EVENT_KEYBOARD_KEY){
-		struct libinput_event_keyboard* kb;
-		kb = libinput_event_get_keyboard_event(event);
-		int kbKey = libinput_event_keyboard_get_key(kb);
-		int kbState = libinput_event_keyboard_get_key_state(kb);
-		listener->keyState[kbKey] = kbState;
+	if(event != NULL){
+		if(libinput_event_get_type(event) == LIBINPUT_EVENT_KEYBOARD_KEY){
+			struct libinput_event_keyboard* kb;
+			kb = libinput_event_get_keyboard_event(event);
+			int kbKey = libinput_event_keyboard_get_key(kb);
+			int kbState = libinput_event_keyboard_get_key_state(kb);
+			listener->keyState[kbKey] = kbState;
+		}
+		else if(libinput_event_get_type(event) == LIBINPUT_EVENT_POINTER_MOTION){
+			struct libinput_event_pointer* pointer;
+			pointer = libinput_event_get_pointer_event(event);
+			listener->mouseDX = libinput_event_pointer_get_dx(pointer);
+			listener->mouseDY = libinput_event_pointer_get_dy(pointer);
+		}
+		else if(libinput_event_get_type(event) == LIBINPUT_EVENT_POINTER_BUTTON){
+			struct libinput_event_pointer* pointer;
+			pointer = libinput_event_get_pointer_event(event);
+			int buttonState = libinput_event_pointer_get_button_state(pointer);
+			switch(libinput_event_pointer_get_button(pointer)){
+				case BTN_LEFT: listener->mbState[0] = buttonState; break;
+				case BTN_MIDDLE: listener->mbState[1] = buttonState; break;
+				case BTN_RIGHT: listener->mbState[2] = buttonState; break;
+				default: break;
+			};
+		}
 	}
 
 	libinput_event_destroy(event);
@@ -82,6 +123,36 @@ COLANDERDEF int colander_getKeyPressed(ColanderListener listener, int key){
 }
 COLANDERDEF int colander_getKeyReleased(ColanderListener listener, int key){
 	return listener.keyState[key] != listener.oldKeyState[key] && listener.keyState[key] == 0;
+}
+
+COLANDERDEF int colander_getMouseDX(ColanderListener listener){ return listener.mouseDX; }
+COLANDERDEF int colander_getMouseDY(ColanderListener listener){ return listener.mouseDY; }
+COLANDERDEF int colander_getMouseButton(ColanderListener listener, int button){
+	switch(button){
+		case BTN_LEFT: return listener.mbState[0];
+		case BTN_MIDDLE: return listener.mbState[1];
+		case BTN_RIGHT: return listener.mbState[2];
+		default: break;
+	}
+	return 0;
+}
+COLANDERDEF int colander_getMouseButtonPressed(ColanderListener listener, int button){
+	switch(button){
+		case BTN_LEFT: return listener.mbState[0] != listener.oldMbState[0] && listener.mbState[0] == 1;
+		case BTN_MIDDLE: return listener.mbState[1] != listener.oldMbState[1] && listener.mbState[1] == 1;
+		case BTN_RIGHT: return listener.mbState[2] != listener.oldMbState[2] && listener.mbState[2] == 1;
+		default: break;
+	}
+	return 0;
+}
+COLANDERDEF int colander_getMouseButtonReleased(ColanderListener listener, int button){
+	switch(button){
+		case BTN_LEFT: return listener.mbState[0] != listener.oldMbState[0] && listener.mbState[0] == 0;
+		case BTN_MIDDLE: return listener.mbState[1] != listener.oldMbState[1] && listener.mbState[1] == 0;
+		case BTN_RIGHT: return listener.mbState[2] != listener.oldMbState[2] && listener.mbState[2] == 0;
+		default: break;
+	}
+	return 0;
 }
 
 #endif // COLANDER_IMPLEMENTATION
